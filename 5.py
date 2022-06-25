42e209e07955e0a2d96a1797b207e14ecc118c54
Askarikus <askarikus@yandex.ru>
2022-06-26 01:01:13 +0500

    second commit, 5 task faster

--- 5.py
+++ 5.py
@@ -1,31 +1,49 @@
 from functools import reduce
+from math import sqrt
 from operator import mul
 
 
-def primes(numb: int) -> set:
-    _set = set()
-    for i in range(numb - 1, 1, -1):
-        is_simple = 0
+def is_prime(num):
+    if num <= 1:
+        return False
+    i = 2
+    while i <= sqrt(num):
+        if num%i == 0:
+            return False
+        i += 1
+    return True
+
+def primes(numb, primesl):
+    i = 2
+    while i <= (1 + (numb/2)):
         if numb % i == 0:
-            for j in range(i - 1, 1, -1):
-                if i % j == 0:
-                    is_simple = is_simple + 1
-            if is_simple == 0:
-                _set.add(i)
-    return _set
+            if is_prime(i):
+                if i not in primesl:
+                    return False
+        i += 1
+    return True
 
 
 def count_find_num(primesl, lim):
-    minimum: int = reduce(mul, primesl)
+    minimum = reduce(mul, primesl)
     _set = set()
-    for i in range(minimum, lim + 1):
+    for i in range(minimum, lim + 1, minimum):
         if sum(1 for prime in primesl if not i % prime) == len(primesl):
-            if primes(i) == set(primesl):
+            if primes(i, primesl):
                 _set.add(i)
     return [len(_set), max(_set)] if _set else []
 
 
 if __name__ == '__main__':
+    # primesL = [2, 3]
+    # limit = 200
+    # print(count_find_num(primesL, limit))
+
+    primesL = [41, 59, 89]
+    limit = 14731875
+    print(count_find_num(primesL, limit))
+    # result is [3, 12702169]
+
     primesL = [2, 3]
     limit = 200
     assert count_find_num(primesL, limit) == [13, 192]
