a682183d0199ed5ab0fbb07db2c194d2a59e4b4c
Askarikus <askarikus@yandex.ru>
2022-06-26 21:30:56 +0500

    all 5 tasks completed

--- 5.py
+++ 5.py
@@ -1,51 +1,25 @@
+from itertools import combinations_with_replacement
 from functools import reduce
-from math import sqrt
 from operator import mul
-
-set_of_primes = {2}
-
-
-def is_prime(num):
-    if num <= 1:
-        return False
-    i = 2
-    sqrt_num = sqrt(num)
-    while i <= sqrt_num:
-        if num % i == 0:
-            return False
-        i += 1
-    return True
-
-
-def primes(number, input_list_of_primes):
-    _set_of_primes_divisors = set()
-    for element in set_of_primes:
-        if number % element == 0:
-            if element not in input_list_of_primes:
-                return False
-            else:
-                _set_of_primes_divisors.add(element)
-    return _set_of_primes_divisors == set(input_list_of_primes)
 
 
 def count_find_num(input_list_of_primes, up_limit):
     divisors_set_result = set()
+    min_prime = min(input_list_of_primes)
+    len_permutations = sum(1 for i in range(1, 100) if min_prime ** i <= up_limit) + 1
     minimum_divisor = reduce(mul, input_list_of_primes)
-    if (sqrt(up_limit) + 100) > max(set_of_primes):
-        for n in range(int(sqrt(up_limit) + 100), max(set_of_primes), -1):
-            if is_prime(n):
-                set_of_primes.add(n)
-    for i in range(minimum_divisor, up_limit + 1, minimum_divisor):
-        if primes(i, input_list_of_primes):
-            divisors_set_result.add(i)
+    input_list_of_primes.append(1)
+    all_comb = combinations_with_replacement(input_list_of_primes, len_permutations)
+    input_list_of_primes.remove(1)
+    for i in all_comb:
+        if minimum_divisor <= reduce(mul, i) <= up_limit:
+            if (set(i).symmetric_difference(set(input_list_of_primes)) == {1} or
+                    set(i).symmetric_difference(set(input_list_of_primes)) == set()):
+                divisors_set_result.add(reduce(mul, i))
     return [len(divisors_set_result), max(divisors_set_result)] if divisors_set_result else []
 
 
 if __name__ == '__main__':
-    primesL = [2, 3]
-    limit = 200
-    assert count_find_num(primesL, limit) == [13, 192]
-
     primesL = [2, 5]
     limit = 200
     assert count_find_num(primesL, limit) == [8, 200]
